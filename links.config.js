995116b69bfea0f4621b07a3f59bac8a73728ad1
nasimbinjasim <nasimbinjasim@gmail.com>
2026-09-05 23:42:20 +0600

    linktree added

--- links.config.js
+++ links.config.js
@@ -6,5 +6,6 @@ module.exports = {
   whatsapp: "https://wa.me/8801714837777?text=Hi%2C%20I%27d%20like%20to%20talk%20to%20You",
   telegram: "https://t.me/nasimbinjasim",
   fb: "https://facebook.com/nasimbinjasim",
+  link: "https://linktr.ee/nasimbinjasim",
   // add more: slug: "https://destination-url.com"
 };
